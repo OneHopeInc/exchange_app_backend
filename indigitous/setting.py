a89d64f3fbe52b699c95c4bf616bbc3f847d0675
Enrique Pinedo <enrjon@gmail.com>
2020-05-04 16:21:52 -0400

    updated config variables

--- indigitous/setting.py
+++ indigitous/setting.py
@@ -1,8 +1,10 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+DATABASE_URL = os.environ.get('DATABASE_URL')
+#'postgres://mvfvspgirbpxkr:d47520ce55afaf9efb82c14a0d8b6873960c36f374135958fabb3f5512ef682c@ec2-52-203-160-194.compute-1.amazonaws.com:5432/dfp63ea3ndf4ll'
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+#'c31bfb18b23c22eda77ba58894f1fea9a9115db45acd6bc1'
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
