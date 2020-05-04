7e9c952ef9c27a2c3b7afeddcd4c0ddac4f0bd1b
Enrique Pinedo <enrjon@gmail.com>
2020-05-04 17:00:13 -0400

    updated settings with django; updated reqs

--- indigitous/setting.py
+++ indigitous/setting.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 #'postgres://mvfvspgirbpxkr:d47520ce55afaf9efb82c14a0d8b6873960c36f374135958fabb3f5512ef682c@ec2-52-203-160-194.compute-1.amazonaws.com:5432/dfp63ea3ndf4ll'
@@ -11,3 +12,6 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+django_heroku.settings(locals())
