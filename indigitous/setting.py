dc737866a0bd1cf4dd9bb5d8988529531f31ab67
Enrique Pinedo <enrjon@gmail.com>
2020-05-04 16:05:38 -0400

    updated static and media paths; added csv to gitignore

--- indigitous/setting.py
+++ indigitous/setting.py
@@ -3,3 +3,9 @@ import os
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
